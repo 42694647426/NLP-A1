594e79638401b2fc8a3ec85726dd231823cbad6a
42694647426 <hyyshao@gmail.com>
2020-10-05 15:58:35 -0400

    add more models(Logistic, SVM)

--- a1.py
+++ a1.py
@@ -1,9 +1,10 @@
-
 import nltk
 import pandas as pd
 import matplotlib
 matplotlib.use('agg')
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
 import string
 import random
 from nltk.corpus import stopwords
@@ -16,6 +17,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.naive_bayes import GaussianNB
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.naive_bayes import MultinomialNB,BernoulliNB
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.naive_bayes import MultinomialNB,BernoulliNB
+from sklearn.linear_model import LogisticRegression,SGDClassifier
 
 # load text data
 df_neg = pd.read_csv('rt-polaritydata\\rt-polarity.neg',sep='\t', encoding='latin-1', header=None)
@@ -26,7 +35,9 @@ df_pos.columns = ['Reviews']
 df_neg['Sentiment'] = "neg"
 df_pos['Sentiment'] = "pos"
 
+
 df = pd.concat([df_neg, df_pos])
+print("Original dataset: ")
 print(df[:4])
 
 
@@ -41,96 +52,108 @@ df['Reviews'] = df['Reviews'].apply(lambda x: remove_punc(x))
 tokenizer = RegexpTokenizer(r'\w+')
 df['Reviews'] = df['Reviews'].apply(lambda x: tokenizer.tokenize(x.lower()))
 
+# remove punctions and tokenize sentiences are basic preprocessing steps, then test if we need to remove stop words
+#lemmatize, stem, words. et. using pipline. 
+
+
 #remove stop words
 def remove_stopwords(words):
     word = [w for w in words if w not in stopwords.words('english')]
     return word
-
-df['Reviews'] = df['Reviews'].apply(lambda x: remove_stopwords(x))
+#df['Reviews'] = df['Reviews'].apply(lambda x: remove_stopwords(x))
 
 #lemmatization
 lemmatizer = WordNetLemmatizer()
 def lemmatize_text(word):
     lem = [lemmatizer.lemmatize(i) for i in word]
     return lem
-df['Reviews'] = df['Reviews'].apply(lambda x: lemmatize_text(x))
+#df['Reviews'] = df['Reviews'].apply(lambda x: lemmatize_text(x))
 
 #stemmatization
 stemmer = PorterStemmer()
 def stem_text(word):
     stem_word = [stemmer.stem(i) for i in word]
     return stem_word
-df['Reviews'] = df['Reviews'].apply(lambda x: stem_text(x))
+#df['Reviews'] = df['Reviews'].apply(lambda x: stem_text(x))
+
+# automatize the preprocessing
+def processing(df, stem=True, lemma = True, stop = True):
+    if(stem):
+        df['Reviews'] = df['Reviews'].apply(lambda x: stem_text(x))
+    if(lemma):
+        df['Reviews'] = df['Reviews'].apply(lambda x: lemmatize_text(x))
+    if(stop):
+        df['Reviews'] = df['Reviews'].apply(lambda x: remove_stopwords(x))
+    return(df)
+
+df = processing(df)
+print("Processed dataset: ")
 print(df[:5])
 
-# shuffle the reviews 
-df = shuffle(df)
-word_pos= []
-all_words = []
-for s in df['Reviews']:
-    word_pos.append(nltk.pos_tag(s))
-
-print(word_pos[:5])
-allowed_type = ["JJ", "JJR", "JJS","NN"]
-words=[]
-
-for i in word_pos:
-    for j in i:
-        words.append(j)
-#print("words: ")
-#print(words[:5])
-for w in words:
-        if w[1] in allowed_type:
-            all_words.append(w[0])
-#print("adjs: ")
-#print(all_words[:5])
-
-
-#count word frequencies 
-all_words = nltk.FreqDist(all_words)
-
-#select features 
-features = list(all_words)[:500]
-#print(features[:5])
-
 #build feature set
-def find_features(text):
+def find_features(text, features):
     res = {}
     for w in features:
         res[w] = (w in text)
     return res
 
-X_set = []
-Y_set = []
-for index, row in df.iterrows():
-    X_set.append(find_features(row['Reviews']))
-    Y_set.append(row['Sentiment']) 
-featuresets = [(find_features(row['Reviews']), row['Sentiment']) for index, row in df.iterrows()]
+def feature_set(df, N=500): #N = number of features to be selected
+    # shuffle the reviews 
+    df = shuffle(df)
+    word_pos= []
+    all_words = []
+    for s in df['Reviews']:
+        word_pos.append(nltk.pos_tag(s)) # add tags to every word
+    allowed_type = ["JJ", "JJR", "JJS","NN"] #allow adjectives and Nouns not adverbs or verbs. 
+    words=[] #unlist the words array
+    for i in word_pos:
+        for j in i:
+            words.append(j)
+    for w in words:
+        if w[1] in allowed_type:
+            all_words.append(w[0])
+    all_words = nltk.FreqDist(all_words) #count frequency of words
+    features = list(all_words)[:N] # extract the 500 most frequent words as features
+    featuresets = [(find_features(row['Reviews'], features), row['Sentiment']) for index, row in df.iterrows()]
+    return featuresets
+
+featuresets = feature_set(df)
 #split test train set
 #random.shuffle(featuresets)
 train_set = featuresets[:7463]
 test_set = featuresets[7463:]
+
 #split train set test set
+X_set = []
+Y_set = []
+for index, row in df.iterrows():
+    X_set.append(find_features(row['Reviews'], features))
+    Y_set.append(row['Sentiment']) 
 X_train, X_test, Y_train, Y_test = train_test_split(X_set, Y_set, test_size = 0.3, random_state = True)
 print(X_train[:10])
 
 #fit naive bayes model
-
 NaiveBayes = nltk.NaiveBayesClassifier.train(train_set)
 
 print("Navie Bayes accuracy: ",nltk.classify.accuracy(NaiveBayes, test_set) )
 
 NaiveBayes.show_most_informative_features(15)
+
+#Logistic regression 
+
+LogReg_clf = SklearnClassifier(LogisticRegression())
+LogReg_clf.train(train_set)
+
+print("Logistic Regression accuracy: ",nltk.classify.accuracy(LogReg_clf, test_set) )
+
+#support vector machine
+SVM_clf = SklearnClassifier(SVC())
+SVM_clf.train(train_set)
+print("Support Vector Machine accuracy: ",nltk.classify.accuracy(SVM_clf, test_set) )
+
 #f1 = f1_score(labels=['neg', 'pos'], average='micro')
 #print(f1)
 
-clf = GaussianNB()
-clf.fit(X_train, Y_train)
-
-plot_confusion_matrix(clf, X_test, Y_test)  
-plt.show() 
-
-#all_words = [(R, S) for R, S in df.iterrows()]
 
 #cv = CountVectorizer()
 #text_count = cv.fit_transform(df['Reviews'])
