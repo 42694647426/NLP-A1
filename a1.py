5f7e7dcd69ad0d601fad639287d4150ecd30d282
42694647426 <hyyshao@gmail.com>
2020-10-06 21:27:35 -0400

    Add word cloud plot

--- a1.py
+++ a1.py
@@ -1,7 +1,6 @@
 import nltk
 import pandas as pd
 import matplotlib
-matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import string
 import random
@@ -23,7 +22,14 @@ from sklearn.pipeline import Pipeline
 from nltk.classify.scikitlearn import SklearnClassifier
 from sklearn.naive_bayes import MultinomialNB,BernoulliNB
 from sklearn.linear_model import LogisticRegression,SGDClassifier
-
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+from wordcloud import WordCloud
+import cv2 
 # load text data
 df_neg = pd.read_csv('rt-polaritydata\\rt-polarity.neg',sep='\t', encoding='latin-1', header=None)
 df_pos = pd.read_csv('rt-polaritydata\\rt-polarity.pos',sep='\t', encoding='latin-1', header=None)
@@ -33,12 +39,9 @@ df_pos.columns = ['Reviews']
 df_neg['Sentiment'] = "neg"
 df_pos['Sentiment'] = "pos"
 
-
-
 df = pd.concat([df_neg, df_pos])
 print("Original dataset: ")
 print(df[:4])
-
 
 #remove punctuations 
 def remove_punc(text):
@@ -51,11 +54,34 @@ df['Reviews'] = df['Reviews'].apply(lambda x: remove_punc(x))
 tokenizer = RegexpTokenizer(r'\w+')
 df['Reviews'] = df['Reviews'].apply(lambda x: tokenizer.tokenize(x.lower()))
 
-# remove punctions and tokenize sentiences are basic preprocessing steps, then test if we need to remove stop words
-#lemmatize, stem, words. et. using pipline. 
-
 unprocessed_df = df
 
+
+#plot cloud graph
+df_neg['Reviews']= df_neg['Reviews'].apply(lambda x: remove_punc(x))
+df_pos['Reviews']= df_pos['Reviews'].apply(lambda x: remove_punc(x))
+
+text_neg = ' '.join(df_neg['Reviews'])
+text_pos = ' '.join(df_pos['Reviews'])
+wordcloud_neg = WordCloud(background_color='white',max_font_size = 50,stopwords=stopwords.words('english'),
+        max_words=150,
+        scale=3,
+        random_state=1).generate(text_neg)
+wordcloud_pos = WordCloud(background_color='white',max_font_size = 50,stopwords=stopwords.words('english'),
+        max_words=150, 
+        scale=3,
+        random_state=1).generate(text_pos)
+plt.figure(figsize = (15, 9))
+# Display the generated image:
+
+#plt.imshow(wordcloud_neg,interpolation= "bilinear")
+plt.imshow(wordcloud_pos,interpolation= "bilinear")
+plt.axis("off")
+plt.show()
+
+
+# remove punctions and tokenize sentiences are basic preprocessing steps, then test if we need to remove stop words
+#lemmatize, stem, words. et. using pipline. 
 #remove stop words
 def remove_stopwords(words):
     word = [w for w in words if w not in stopwords.words('english')]
@@ -86,11 +112,7 @@ def processing(df, stem=True, lemma = True, stop = True):
         df['Reviews'] = df['Reviews'].apply(lambda x: remove_stopwords(x))
     return(df)
 
-'''
-df = processing(df)
-print("Processed dataset: ")
-print(df[:5])
-'''
+
 #build feature set
 def find_features(text, features):
     res = {}
@@ -98,14 +120,15 @@ def find_features(text, features):
         res[w] = (w in text)
     return res
 
-def feature_set(df, N=500): #N = number of features to be selected
+def feature_set(df, N=1000): #N = number of features to be selected
     # shuffle the reviews 
     df = shuffle(df)
     word_pos= []
     all_words = []
     for s in df['Reviews']:
         word_pos.append(nltk.pos_tag(s)) # add tags to every word
-    allowed_type = ["JJ", "JJR", "JJS","NN"] #allow adjectives and Nouns not adverbs or verbs. 
+    allowed_type = ["JJ", "JJR", "JJS","NN", 
+    "NNS", "NNP", "NNPS", "VB", "VBD", "VBG", "VBN"] #allow adjectives, Nouns and verbs not adverbs, conjunctions 
     words=[] #unlist the words array
     for i in word_pos:
         for j in i:
@@ -117,13 +140,7 @@ def feature_set(df, N=500): #N = number of features to be selected
     features = list(all_words)[:N] # extract the 500 most frequent words as features
     featuresets = [(find_features(row['Reviews'], features), row['Sentiment']) for index, row in df.iterrows()]
     return featuresets
-'''
-featuresets = feature_set(df)
-#split test train set
-#random.shuffle(featuresets)
-train_set = featuresets[:7463]
-test_set = featuresets[7463:]
-'''
+
 #split train set test set
 '''
 X_set = []
@@ -168,8 +185,18 @@ def test_preprocess(df = unprocessed_df, stem=True, lemma = True, stop = True):
         best_model = "Support Vector Machine"
     print("Support Vector Machine accuracy: ", SVM_acc)
 
+    #MLP Neural Network
+    DT_clf = SklearnClassifier(DecisionTreeClassifier())
+    DT_clf.train(train_set)
+    DT_acc = nltk.classify.accuracy(DT_clf, test_set)
+    if(DT_acc>best_acc):
+        best_acc = DT_acc
+        best_model = "Decision Tress"
+    print("Decision Tree accuracy: ", DT_acc)
+
     print("Best model is", best_model, ", Best accuracy: ", best_acc)
     return best_acc
+
 
 def run_test(df =unprocessed_df ):
     l =[]
@@ -209,32 +236,102 @@ def run_test(df =unprocessed_df ):
 
 print(run_test())
 
-'''
-#fit naive bayes model
+# plot confusion matrix
+df = processing(df)
+print("Processed dataset: ")
+print(df[:5])
+
+featuresets = feature_set(df)
+#split test train set
+random.shuffle(featuresets)
+train_set = featuresets[:7463]
+test_set = featuresets[7463:]
+
+
+def plot_confusion_matrix(y_true, y_pred, classes, classifier = "Naive Bayes",
+                          cmap=plt.cm.Blues):
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print("Normalized confusion matrix", classifier)
+
+    print(cm)
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    #show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title='Normalized confusion matrix for ' + classifier,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+    # Loop over data dimensions and create text annotations.
+    fmt = '.5f'  
+    thresh = cm.max() / 2+0.1
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
+np.set_printoptions(precision=10)
+
 NaiveBayes = nltk.NaiveBayesClassifier.train(train_set)
-
-print("Navie Bayes accuracy: ",nltk.classify.accuracy(NaiveBayes, test_set) )
-
+print("Navie Bayes accuracy: ",nltk.classify.accuracy(NaiveBayes, test_set))
 NaiveBayes.show_most_informative_features(15)
 
-#Logistic regression 
+ground_truth = [r[1] for r in test_set]
+preds = [NaiveBayes.classify(r[0]) for r in test_set]
 
+y_test = ground_truth
+y_pred = preds
+class_names = ['neg', 'pos']
+
+plot_confusion_matrix(y_test, y_pred, classes=class_names)
+
+#logistic regression
 LogReg_clf = SklearnClassifier(LogisticRegression())
 LogReg_clf.train(train_set)
+LogReg_acc = nltk.classify.accuracy(LogReg_clf, test_set)
+print("Logistic Regression accuracy: ", LogReg_acc)
 
-print("Logistic Regression accuracy: ",nltk.classify.accuracy(LogReg_clf, test_set) )
+preds = [LogReg_clf.classify(r[0]) for r in test_set]
+y_pred = preds
 
+
+plot_confusion_matrix(y_test, y_pred, classes=class_names, classifier="Logistic Regression")
 #support vector machine
 SVM_clf = SklearnClassifier(SVC())
 SVM_clf.train(train_set)
-print("Support Vector Machine accuracy: ",nltk.classify.accuracy(SVM_clf, test_set) )
+SVM_acc = nltk.classify.accuracy(SVM_clf, test_set)
+print("Support Vector Machine accuracy: ", SVM_acc)
 
-#f1 = f1_score(labels=['neg', 'pos'], average='micro')
-#print(f1)
+preds = [SVM_clf.classify(r[0]) for r in test_set]
+y_pred = preds
 
 
-#cv = CountVectorizer()
-#text_count = cv.fit_transform(df['Reviews'])
-'''
+plot_confusion_matrix(y_test, y_pred, classes=class_names, classifier="Support Vector Machine")
+
+#MLP Neural Network
+DT_clf = SklearnClassifier(DecisionTreeClassifier())
+DT_clf.train(train_set)
+DT_acc = nltk.classify.accuracy(DT_clf, test_set)
+print("Decision Tree accuracy: ", DT_acc)
+
+preds = [DT_clf.classify(r[0]) for r in test_set]
+y_pred = preds
+
+plot_confusion_matrix(y_test, y_pred, classes=class_names, classifier="Decision Tree")
+
+plt.show()
 
 
